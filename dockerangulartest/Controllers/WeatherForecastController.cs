104feb1fb3f7b9118f848c4c24185767ba9cdc0a
kennydust <kennydust@gmail.com>
2020-09-06 12:46:12 -0400

    initial

---
+++ dockerangulartest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace dockerangulartest.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<WeatherForecast>> Get()
+		{
+			/*
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			})
+			.ToArray();
+			*/
+
+			using (var client = new HttpClient())
+			{
+
+
+				//var response = await client.GetAsync("http://localhost:62004/weatherforecast");
+				var response = await client.GetAsync("http://apitest/weatherforecast");
+				if (response.IsSuccessStatusCode)
+				{
+					var json = await response.Content.ReadAsStringAsync();
+					var forecasts = JsonConvert.DeserializeObject<List<WeatherForecast>>(json);
+					return forecasts;
+				}
+			}
+
+			return null;
+		}
+	}
+}
